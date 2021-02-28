0da7d46c8d672b7901a1e319d69be6e68a1b1bc8
Oscar Mugambi <oscarmugambi@gmail.com>
2021-02-28 11:54:07 +0300

    whole portfolio

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
         alt="background"
         className="absolute object-cover w-ful h-full"
       />
-      <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+      <section className="relative flex justify-center min-h-screen pt-12 lg:pt-20 px-8">
         <h1 className="text-6xl text-green-900 font-bold cursive leading-none lg:leading-snug home-name">
           Hi there. I'm Oscar.
         </h1>
